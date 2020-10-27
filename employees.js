ec4aba177b5e2e986aea541f515b95bd0e37ddce
sarah-morgenstein <sarah.morgenstein@tri-c.edu>
2020-10-26 23:30:46 -0400

    Initial commit

---
+++ employees.js
@@ -0,0 +1,24 @@
+var staff = {
+  "employees": [{
+      "id": "1",
+      "firstName": "Cookie",
+      "lastName": "Monster",
+      "photo": "images/employee-1.jpg",
+      "position": "baker"
+    },
+    {
+      "id": "2",
+      "firstName": "Kermit",
+      "lastName": "Frog",
+      "photo": "images/employee-2.jpg",
+      "position": "store manager"
+    },
+    {
+      "id": "3",
+      "firstName": "Big",
+      "lastName": "Bird",
+      "photo": "images/employee-3.jpg",
+      "position": "produce stocker"
+    }
+  ]
+};
---
+++ script.js
@@ -0,0 +1,2 @@
+// hint - this is how you get the current date/time
+//  var today= new Date();
